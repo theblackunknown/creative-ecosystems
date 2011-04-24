1172854244ba77a24cfae4f5213d256b52b5935d
MACHIZAUD Andrea <andrea.machizaud.pro@gmail.com>
2011-04-24 22:51:17 +0200

    Basic template to create an application

---
+++ draughtsman-ecosystem/src/main/java/org/blackpanther/ecosystem/DraughtsEnvironment.java
@@ -0,0 +1,21 @@
+package org.blackpanther.ecosystem;
+
+/**
+ * @author MACHIZAUD Andréa
+ * @version 1.0 - 4/24/11
+ */
+public class DraughtsEnvironment extends Environment {
+    /**
+     * {@inheritDoc}
+     */
+    public DraughtsEnvironment(final int width, final int height) {
+        super(width, height);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DraughtsEnvironment(final int size) {
+        super(size);
+    }
+}
